f0534643fc80fc00d15b7e971901769c5ff1ecb4
Sharpoen <kenaniahmc@gmail.com>
2023-05-14 22:53:44 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def qrcode_maker(data):
     image_dis.configure(image=qr_image)
     image_dis.image=qr_image
 
-def save_qrcode():
+def save_qrcode(*burn):
     code_image = Image.open("QRCode.png")
     file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG", "*.png")])
     code_image.save(f"{file_path}")
@@ -33,17 +33,20 @@ image_dis = Label(root, image=qr_image)
 #image_dis.pack()
 
 #Text Entry
-entry = Text(root, height=1, width=1, yscrollcommand=True)
+# entry = Text(root, height=5, width=1, yscrollcommand=True)
+entry = Entry(root)
 def enter(event):
-    qrcode_maker(list(entry.get(1.0, "end-1c")))
+    qrcode_maker(list(entry.get()))
+    s_button.focus()
 entry.bind("<Return>", enter)
 #entry.pack()
-entry.get(1.0, "end-1c")
+entry.get()
 #Enter Button
-e_button = Button(root, text="Create", command=lambda: qrcode_maker(entry.get(1.0, "end-1c")))
+e_button = Button(root, text="Create", command=lambda: qrcode_maker(entry.get()))
 
 #Save Button
 s_button = Button(root, text="Save", command=save_qrcode)
+s_button.bind("<Return>", save_qrcode)
 
 #Display stuff
 image_dis.grid(column=0, row=0, columnspan=2, sticky="nesw", padx=5, pady=5)
@@ -51,11 +54,13 @@ entry.grid(column=0, row=1, columnspan=2, sticky="ew", padx=5)
 e_button.grid(column=0, row=3, sticky="ew", padx=5, pady=5)
 s_button.grid(column=1, row=3, sticky="ew", padx=5, pady=5)
 
+entry.focus()
 
 
+# def keypress(event):
+#     print([event.char])
+# root.bind("<Key>", keypress)
 
 root.mainloop()
-
-
 
 
